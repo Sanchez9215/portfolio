80450a28ce3bd805f25f647b2af1b114b73bffa3
Edgar Sanchez <edgar.sanchez9215@gmail.com>
2023-05-31 13:39:10 -0700

    react nav progress

---
+++ react-nav-bar/src/index.js
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "about",
+    element: <About />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
